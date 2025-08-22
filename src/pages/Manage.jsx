b490cbb226f7b2a1b28ef83be53cb7ec90b577ce
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-08-22 23:25:30 +0530

    Questions

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -32,7 +32,7 @@ function Manage() {
 
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
-            <div className="flex justify-end mb-5.5">
+            <div className="flex justify-end mb-5">
                 <button onClick={() => setIsAddModalOpen(!isAddModalOpen)}
                     className="cursor-pointer flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md shadow transition duration-200"
                 >
